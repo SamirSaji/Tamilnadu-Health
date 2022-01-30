e757fd8a5240e2f3a221fddefa1095326124e806
MohammedSamir <51956976+SamirSaji@users.noreply.github.com>
2022-01-30 10:00:10 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,15 +1,15 @@
 if (process.env.NODE_ENV === "DEVELOPMENT") {
   const configuration = {
-    apiURL: "http://206.189.128.57:7080",
-    masterRegisterEndpoint: "https://master.uhcit.in/graphql",
-    downloadEndPoint: "https://206.189.139.120/graphql"
+    apiURL: "",
+    masterRegisterEndpoint: "",
+    downloadEndPoint: ""
   };
   module.exports = configuration;
 } else {
   module.exports = {
-    apiURL: "https://api-dev.uhcitp.in",
-    masterRegisterEndpoint: "https://mr.subscriblee.com/graphql",
-    downloadEndPoint: "https://206.189.139.120/graphql"
+    apiURL: "",
+    masterRegisterEndpoint: "",
+    downloadEndPoint: ""
 
   };
 }
